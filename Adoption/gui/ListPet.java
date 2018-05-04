cf1cfa99c6f725280b72b1cc413bd8a7e88d9f5a
Unleashz <mohamed.khadhr@esprit.tn>
2018-05-04 14:13:49 -0700

    Adoption files

---
+++ Adoption/gui/ListPet.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gui;
+
+import com.codename1.components.ImageViewer;
+import com.codename1.components.SpanLabel;
+import com.codename1.ui.Button;
+import com.codename1.ui.Container;
+import com.codename1.ui.Form;
+import com.codename1.ui.Image;
+import com.codename1.ui.Label;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.plaf.Border;
+import com.codename1.ui.util.Resources;
+import com.mycompany.Entitie.Pet;
+import com.mycompany.Service.ServicePet;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author mac
+ */
+public class ListPet {
+    
+    Form  f; 
+ 
+    public ListPet(Resources res)  {
+        f=new Form("Liste Des Pets",BoxLayout.y());
+     
+           
+         
+        //*******************Recuperer la liste de produit de la base***************************************************
+        
+           ServicePet sv=new ServicePet();
+           ArrayList<Pet> list=sv.showList();
+           
+          
+       //********************************Parcourir la liste**************************************************************** 
+        for(Pet v : list)
+        {  
+               Container c1 =new Container(BoxLayout.x());
+               Container c3 =new Container(BoxLayout.y());
+               
+              c1.getStyle().setBorder(Border.createLineBorder(2));
+              c1.getStyle().setMargin(1, 1, 1, 1);
+              c1.getStyle().setPadding(20, 20, 0, 0);
+
+        //****************************les elements du containers********************************************************
+               
+               ImageViewer iv=new ImageViewer();
+               
+           try {
+               iv.setImage(Image.createImage("/"+ v.getPet_image()).scaled(100, 100));
+           } catch (IOException ex) {
+                    System.out.println("err");
+           }
+              
+               //Button b=new Button(image2.scaled(30,30));
+               Button b=new Button("View Pet");
+               
+                       
+               Label l1=new Label("id: "+ v.getId_pet());
+              
+
+             //  l1.getStyle().set(0xC40C0C);
+             
+               c1.add(iv);
+               c1.add(c3);
+               c3.add(new SpanLabel( v.getName_pet()));
+               c3.add(l1);
+               
+               c3.add(b);
+               
+               
+            //*******************************Action sur le bouton add*****************************************************
+               
+//                   b.addActionListener(e->{ 
+//                       try {
+//                           showUnPet sc=new showUnPet().show();
+//    //                         showUnPet sc=new showUnPet(v.getId_pet());
+//    
+////                           sc.getF().show();//showUnPediatre(p.getId());
+//                       } catch (IOException ex) {
+//                           //Logger.getLogger(ListePediatre.class.getName()).log(Level.SEVERE, null, ex);
+//                       }
+//                   
+//                   });
+
+               int id = v.getId_pet();
+               
+               System.out.println(id);
+               
+               b.addActionListener(e -> {
+                   new showUnPet(res, id).show();
+               });
+       
+           
+            f.add(c1);
+            
+        }
+         
+        
+    }
+
+    public Form getF() {
+        return f;
+    }
+
+    public void setF(Form f) {
+        this.f = f;
+    }
+    
+}
---
+++ Adoption/gui/showUnPet.java
@@ -0,0 +1,120 @@
+package com.mycompany.gui;
+
+//import java.util.logging.Logger;
+//import java.util.logging.Level;
+import com.codename1.components.ImageViewer;
+import com.codename1.components.ScaleImageLabel;
+import com.codename1.components.SpanLabel;
+import com.codename1.ui.Button;
+import com.codename1.ui.CheckBox;
+import com.codename1.ui.Component;
+import static com.codename1.ui.Component.BOTTOM;
+import com.codename1.ui.Container;
+import com.codename1.ui.Display;
+import com.codename1.ui.Form;
+import com.codename1.ui.Image;
+import com.codename1.ui.Label;
+import com.codename1.ui.TextField;
+import com.codename1.ui.Toolbar;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.layouts.GridLayout;
+import com.codename1.ui.layouts.LayeredLayout;
+import com.codename1.ui.plaf.Border;
+import com.codename1.ui.plaf.Style;
+import com.codename1.ui.util.Resources;
+import com.mycompany.Entitie.Pet;
+import com.mycompany.Service.ServicePet;
+import java.io.IOException;
+
+
+/**
+ *
+ * @author mac
+ */
+public class showUnPet extends BaseForm{
+    
+    public showUnPet(Resources res, int id) {
+        super("Newsfeed", BoxLayout.y());
+        
+        ServicePet sc1=new ServicePet();
+        Pet p=sc1.showPet(id);
+        
+        
+        
+        Toolbar tb = new Toolbar(true);
+        setToolbar(tb);
+        getTitleArea().setUIID("Container");
+        setTitle("Profile");
+        getContentPane().setScrollVisible(false);
+        
+        super.addSideMenu(res);
+        
+        tb.addSearchCommand(e -> {});
+        
+        
+        Image img = res.getImage("profile-background.jpg");
+//        Image img = Image.createImage("/img.jpg");
+
+        
+        
+        System.out.println(p.getPet_image());
+      
+        if(img.getHeight() > Display.getInstance().getDisplayHeight() / 3) {
+            img = img.scaledHeight(Display.getInstance().getDisplayHeight() / 3);
+        }
+        ScaleImageLabel sl = new ScaleImageLabel(img);
+        sl.setUIID("BottomPad");
+        sl.setBackgroundType(Style.BACKGROUND_IMAGE_SCALED_FILL);
+
+        Label facebook = new Label("786 followers", res.getImage("facebook-logo.png"), "BottomPad");
+        Label twitter = new Label("486 followers", res.getImage("twitter-logo.png"), "BottomPad");
+        facebook.setTextPosition(BOTTOM);
+        twitter.setTextPosition(BOTTOM);
+        
+        try {
+            add(LayeredLayout.encloseIn(
+                    sl,
+                    BorderLayout.south(
+                            GridLayout.encloseIn(3,
+                                    facebook,
+                                    FlowLayout.encloseCenter(
+                                            new Label(Image.createImage("/img.jpg").scaled(100, 100), "PictureWhiteBackgrond")),
+                                    twitter
+                            )
+                    )
+            ));
+        } catch (IOException ex) {
+            //Logger.getLogger(showUnPet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+
+
+        Label username = new Label(p.getName_pet());
+        username.setUIID("TextFieldBlack");
+        addStringValue("name pet", username);
+        
+        Label breed = new Label(p.getBreed());
+        breed.setUIID("TextFieldBlack");
+        addStringValue("Breed", breed);
+        
+        Label sex = new Label(p.getSex());
+        sex.setUIID("TextFieldBlack");
+        addStringValue("Sex", sex);
+        
+        Label age = new Label(""+p.getAge());
+        age.setUIID("TextFieldBlack");
+        addStringValue("Age", age);
+
+        
+
+        
+    }
+    
+    private void addStringValue(String s, Component v) {
+        add(BorderLayout.west(new Label(s, "PaddedLabel")).
+                add(BorderLayout.CENTER, v));
+        add(createLineSeparator(0xeeeeee));
+    }
+}
