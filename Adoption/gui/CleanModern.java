d2f099a59a739ce7f12b7cae283af560c4c56ab4
Unleashz <mohamed.khadhr@esprit.tn>
2018-05-06 16:02:18 +0200

    Update CleanModern.java

--- Adoption/gui/CleanModern.java
+++ Adoption/gui/CleanModern.java
@@ -34,7 +34,6 @@ import com.codename1.ui.Toolbar;
  * of building native mobile applications using Java.
  */
 public class CleanModern {
-    
 
     private Form current;
     private Resources theme;
